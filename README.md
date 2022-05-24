9c2fc6e3d40e77076eb8ab6a2a116051ff7b53f0
Neil Douglas <endy64@icloud.com>
2022-05-24 21:10:21 +0100

    Added render function to diaply the board, put dummy data in the board to aid with a basic CSS starting point

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Tic Tac Toe
 JavaScript Tic Tac Toe game
+Using revealing modular pattern, closure and function factory
--- script.js
+++ script.js
@@ -1,45 +1,75 @@
 const playerFactory = (name) => {
-    const getName = () => name;
-
-    return {getName};
+    return {name};
 };
-
-const neil = playerFactory('neil');
-console.log(neil.getName());
 
 
 
 var gameBoard = (() => {
     'use strict';
-    // var that = this;
+
     var _board = [
-        [0, 0, 0],
-        [0, 0, 0],
-        [0, 0, 0]
+        [ "A", "B", "C" ],
+        [ "D", "E", "F" ],
+        [ "G", "H", "I" ]
     ];
 
-    function _getBoard() {
+    function getBoard() {
         return _board;
     }
 
-    function _setSquare(x, y, marker) {
+    function setSquare(x, y, marker) {
         _board[x][y] = marker;
     }
 
-    function getBoard() {
-        var board = _getBoard();
-        return board;
-    }
-
-    function setSquare(x,y, marker) {
-        _setSquare(x, y, marker);
-    }
-
     return {
-        getBoard: getBoard,
-        setSquare: setSquare
+         getBoard,
+         setSquare
     };
 })();
 
-var temp = gameBoard.getBoard();
-console.log(temp);
+
+// create the players
+const player1 = playerFactory('kate');
+const player2 = playerFactory('neil');
+
+console.log(player1.name);
+
+
+var render = (() => {
+    // cache a copy of the board DOM
+    const divBoard = document.querySelector('[data-name="board"');
+    const div = document.createElement('div');
+    // const p1Move = document.createTextNode('X');
+    // const p2Move = document.createTextNode('O');
+
+
+    // div.appendChild(p1Move);
+
+    // get a copy of the current game board
+    const board = gameBoard.getBoard(); 
+
+    for (var i = 0; i < 3; i++) {
+        for (var j = 0; j < 3; j++) {
+            console.log(board[i][j]);
+            const div = document.createElement('div');
+            const span = document.createElement('span');
+
+            div.dataset.x = i;
+            div.dataset.y = j;
+            div.className = 'square';
+            var content = document.createTextNode(board[i][j]);
+            span.appendChild(content);
+            div.appendChild(span);
+            divBoard.appendChild(div);
+
+        }
+    }
+
+
+    // divBoard.appendChild(div);
+
+    return {
+        divBoard
+    };
+
+})();
